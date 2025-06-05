8c232b6ee461739c3c5e9b265980ad976b9ba250
Biabborges <bianca.lins@sou.inteli.edu.br>
2025-06-05 10:01:04 -0300

    feat: add prometheus integration

--- WebMetric/Program.cs
+++ WebMetric/Program.cs
@@ -1,25 +1,42 @@
 using OpenTelemetry.Metrics;
+using System.Diagnostics.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenTelemetry()
-    .WithMetrics(builder =>
-    {
-        builder.AddPrometheusExporter();
 
-        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics.AddPrometheusExporter();
+
+        metrics.AddMeter("Microsoft.AspNetCore.Hosting",
                          "Microsoft.AspNetCore.Server.Kestrel");
-        builder.AddView("http.server.request.duration",
+
+        metrics.AddView("http.server.request.duration",
             new ExplicitBucketHistogramConfiguration
             {
                 Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
                        0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
             });
+
+        // Adiciona seu Meter customizado
+        metrics.AddMeter("WebMetric.App");
     });
+
 var app = builder.Build();
 
 app.MapPrometheusScrapingEndpoint();
 
-app.MapGet("/", () => "Hello OpenTelemetry! ticks:"
-                     + DateTime.Now.Ticks.ToString()[^3..]);
+// Criação do contador customizado
+var meter = new Meter("WebMetric.App", "1.0.0");
+var requestCounter = meter.CreateCounter<long>("app.request_counter");
+
+// Endpoint que incrementa a métrica
+app.MapGet("/track", () =>
+{
+    requestCounter.Add(1);
+    return Results.Ok("Contador incrementado!");
+});
+
+app.MapGet("/", () => "Hello OpenTelemetry! ticks: " + DateTime.Now.Ticks.ToString()[^3..]);
 
 app.Run();
--- WebMetric/obj/Debug/net8.0/WebMetric.AssemblyInfo.cs
+++ WebMetric/obj/Debug/net8.0/WebMetric.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebMetric")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4083d4a24850c246dd932c3994b346278d3cc005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef7f2d627339a1437bbbef4028d00bbbb0284c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebMetric")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebMetric")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
